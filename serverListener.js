2469aa13c4d29af9639c7d2f80a2a1df98020448
naikyding <74ding@gmail.com>
2022-03-30 14:36:44 +0800

    feat(serverListener): 引入刪除功能

--- serverListener.js
+++ serverListener.js
@@ -1,3 +1,4 @@
+const { deleteList, deleteItem } = require('./todo/delete')
 const {
   getList,
   options,
@@ -8,11 +9,9 @@ const serverListener = (req, res) => {
   if (req.url == "/todos" && req.method == "GET") getList(res)
   else if (req.url == "/todos" && req.method == "POST") {
     // postTodo.js
-  } else if (req.url == "/todos" && req.method == "DELETE") {
-    // deleteTodo.js
-  } else if (req.url.startsWith("/todos/") && req.method == "DELETE") {
-    // deleteTodo.js
-  } else if (req.url.startsWith("/todos/") && req.method == "PATCH") {
+  } else if (req.url == "/todos" && req.method == "DELETE") deleteList(res)
+  else if (req.url.startsWith("/todos/") && req.method == "DELETE") deleteItem(req, res)
+  else if (req.url.startsWith("/todos/") && req.method == "PATCH") {
     // patchTodo.js
   }
   else if (req.method == "OPTIONS") options(res)

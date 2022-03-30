42f7d18726bb7314420180c4e6036dca3c6f2d66
naikyding <74ding@gmail.com>
2022-03-30 13:52:47 +0800

    feat: post 功能 #組員b

---
+++ todo/post.js
@@ -0,0 +1,34 @@
+const { requestBodyBufferHandle } = require('../utils')
+const { v4: uuid } = require('uuid')
+const DB = require('../db')
+
+const {
+  successHandle,
+  errorHandle,
+} = require('../utils/responseHandle')
+
+const postItem = async (req, res) => {
+  try {
+    const { content } = await requestBodyBufferHandle(req)
+
+    DB.push({
+      id: uuid(),
+      content
+    })
+    successHandle({
+      res,
+      statusCode: 201,
+      message: '新增成功',
+      data: DB
+    })
+  } catch {
+    errorHandle({
+      res,
+      message: '資料格式錯誤!'
+    })
+  }
+}
+
+module.exports = {
+  postItem
+}

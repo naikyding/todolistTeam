191c0f64305d925723bbde3e187fecd13d332686
naikyding <74ding@gmail.com>
2022-03-30 11:21:50 +0800

    feat(utils): 請求 body 功能 #組員b

---
+++ utils/index.js
@@ -0,0 +1,17 @@
+/**
+ * 請求 body 處理
+ * @date 2022-03-30
+ * @param {object} req 請求物件
+ * @returns {json} 請求 body 內容 
+ */
+const requestBodyBufferHandle = async (req) => {
+  let buffers = []
+  for await (const buffer of req) {
+    buffers.push(buffer)
+  }
+  return JSON.parse(Buffer.concat(buffers).toString())
+}
+
+module.exports = {
+  requestBodyBufferHandle
+}

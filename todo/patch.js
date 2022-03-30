ae697b3aa4cb68d5d3ecf5f57be4fa6f36971382
naikyding <74ding@gmail.com>
2022-03-30 15:18:07 +0800

    feat: 修改功能 #組員d

--- todo/patch.js
+++ todo/patch.js
@@ -1,20 +1,30 @@
 const DB = require('../db')
-const { errorHandle } = require('../utils/responseHandle')
+const { filterReqId, requestBodyBufferHandle } = require('../utils')
+const { errorHandle, successHandle } = require('../utils/responseHandle')
 
-const editItem = (req, res) => {
-  const { url } = req
-  const id = url.split('/').pop()
-  const editItem = DB.find(item => item.id === id)
-
-  if (!editItem) return errorHandle({
+const editItem = async (req, res) => {
+  const id = filterReqId(req)
+  const DBeditItem = DB.find(item => item.id === id)
+  if (!DBeditItem) return errorHandle({
     res,
     message: '修改失敗，資料有誤或 id 不存在'
   })
 
   try {
+    const { content } = await requestBodyBufferHandle(req)
+    if (!content) throw false
 
+    DBeditItem.content = content
+    successHandle({
+      res,
+      message: '修改成功',
+      data: DB
+    })
   } catch {
-
+    errorHandle({
+      res,
+      message: '修改失敗，資料錯誤'
+    })
   }
 }
 

ebeb5c547ce73f402d7092008c492768f3d0d9b5
NaikyDing <74ding@gmail.com>
2022-04-15 20:17:58 +0800

    feat (server): 建立 todoDB 連線

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const http = require('http')
 const port = process.env.PORT || 3005
+const { todoDBConnect } = require('./todoDB')
+todoDBConnect()
+
 const serverListener = require('./serverListener')
 const server = http.createServer(serverListener)
 
